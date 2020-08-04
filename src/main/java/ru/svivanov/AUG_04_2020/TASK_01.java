6e9bc884072e3536b71d2d89c6d2ca03ddc7d5b0
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-04 13:10:56 +0300

    tripleTrouble

--- src/main/java/ru/svivanov/AUG_04_2020/TASK_01.java
+++ src/main/java/ru/svivanov/AUG_04_2020/TASK_01.java
@@ -1,5 +1,15 @@
 package ru.svivanov.AUG_04_2020;
 
 public class TASK_01 {
-    
+    public static void main(String[] args) {
+        System.out.println(tripleTrouble("123", "123", "123"));
+    }
+
+    public static String tripleTrouble(String one, String two, String three) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < one.length(); i++) {
+            stringBuilder.append(one.charAt(i)).append(two.charAt(i)).append(three.charAt(i));
+        }
+        return stringBuilder.toString();
+    }
 }
