b323236b40597b8383557d70d4db9ef5f1f12e48
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-05 22:12:58 +0300

    small fix

--- src/main/java/ru/svivanov/AUG_05_2020/TASK_03.java
+++ src/main/java/ru/svivanov/AUG_05_2020/TASK_03.java
@@ -2,17 +2,19 @@ package ru.svivanov.AUG_05_2020;
 
 public class TASK_03 {
     public static void main(String[] args) {
-        System.out.println(solve(new int[][]{{1, 2, 3}, {1, 3, 4}, {2, 4, 6}}));
+        System.out.println(solve(new int[][]{{10, -15}, {-1, -3}}));
     }
-    public static int solve(int [][] arr){
+
+    public static int solve(int[][] arr) {
         int max = Integer.MIN_VALUE;
-        for(int j = 0; j < arr[0].length; j++){
+        for (int j = 0; j < arr[0].length; j++) {
             int i = 0;
-            int multi = arr[i][0];
-            for(; i<arr.length; i++){
-                multi *= arr[i][j];
+            int multi = arr[i][j];
+            for (; i < arr.length - 1; i++) {
+                multi *= arr[i + 1][j];
             }
             max = Math.max(multi, max);
-        }return max;
+        }
+        return max;
     }
 }
