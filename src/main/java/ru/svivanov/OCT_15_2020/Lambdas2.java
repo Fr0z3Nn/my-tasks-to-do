92c95ce8c39d501ccfeb21b7890a14147223d39b
Fr0z3Nn <luckyjohn057@gmail.com>
2020-10-15 15:09:16 +0300

    test 2

--- src/main/java/ru/svivanov/OCT_15_2020/Lambdas2.java
+++ src/main/java/ru/svivanov/OCT_15_2020/Lambdas2.java
@@ -1,5 +1,10 @@
 package ru.svivanov.OCT_15_2020;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 interface Mathmatic{
     double calculate(int a);
     static double sqrt(double m){
@@ -21,4 +26,16 @@ class StaticTest{
     static void print(){
         System.out.println("123");
     }
+
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<>();
+        System.out.println(map.put("123", 56));
+        System.out.println(map.put("345",89));
+        System.out.println(map.put("123", 76));
+
+        Set<String> set = new HashSet<>();
+        System.out.println(set.add("private"));
+        System.out.println(set.add("private"));
+    }
+
 }
