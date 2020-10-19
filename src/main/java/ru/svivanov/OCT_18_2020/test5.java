aeb428804947b5268af1ddd064208ff948e2fc72
Fr0z3Nn <luckyjohn057@gmail.com>
2020-10-19 13:26:33 +0300

    test

--- src/main/java/ru/svivanov/OCT_18_2020/test5.java
+++ src/main/java/ru/svivanov/OCT_18_2020/test5.java
@@ -7,5 +7,6 @@ public class test5 {
     public static void main(String[] args) {
         test5();
     }
-    static void test5() { List<Thread> threads = new ArrayList<>(); for (int i = 0; i < 5; i++) { threads.add(new Thread(new ThreadImpl())); } threads.forEach(Thread::start); threads.forEach(t -> { try { t.join(); } catch (InterruptedException ignored) { } });}private static class ThreadImpl implements Runnable { private static int counter = 0; @Override public synchronized void run() { int localValue = ++counter; try { Thread.sleep((long) (Math.random() * 20)); } catch (InterruptedException ignored) { } System.out.println(localValue); }}
+    static void test5() { List<Thread> threads = new ArrayList<>();ThreadImpl ti = new ThreadImpl(); for (int i = 0; i < 5; i++) {
+        threads.add(new Thread(ti)); } threads.forEach(Thread::start); threads.forEach(t -> { try { t.join(); } catch (InterruptedException ignored) { } });}private static class ThreadImpl implements Runnable { private static int counter = 0; @Override public synchronized void run() { int localValue = ++counter; try { Thread.sleep((long) (Math.random() * 20)); } catch (InterruptedException ignored) { } System.out.println(localValue); }}
 }
